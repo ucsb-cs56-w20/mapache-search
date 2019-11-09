6aa589f188930ef4ae5e104b34ca451eb221862d
meredithxu <yiyangxu@umail.ucsb.edu>
2019-11-09 15:57:09 -0800

    MX/added github oauth

--- src/main/java/edu/ucsb/cs56/mapache_search/Application.java
+++ src/main/java/edu/ucsb/cs56/mapache_search/Application.java
@@ -2,6 +2,8 @@ package edu.ucsb.cs56.mapache_search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class Application {
