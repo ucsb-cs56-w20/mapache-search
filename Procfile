40ac47331058ca5795a322ca8e57ee0b00b086fd
Ben Ye <z_ye@ucsb.edu>
2019-11-12 02:46:57 -0800

    committing db files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dserver.port=$PORT -jar target/mapache-search-0.1.0.jar
--- src/main/java/edu/ucsb/cs56/mapache_search/entities/User.java
+++ src/main/java/edu/ucsb/cs56/mapache_search/entities/User.java
@@ -1,4 +1,4 @@
-package edu.ucsb.cs56.mapache_search.user;
+package edu.ucsb.cs56.mapache_search.entities;
 
 //import javax.persistence.Entity;
 //import javax.persistence.GeneratedValue;
