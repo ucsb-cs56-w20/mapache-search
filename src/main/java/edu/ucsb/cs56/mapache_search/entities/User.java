cdb53451d60dce70db97d293445a5d40f39deac1
Ben Ye <z_ye@ucsb.edu>
2019-11-12 01:49:54 -0800

    more incremental changes

--- src/main/java/edu/ucsb/cs56/mapache_search/entities/User.java
+++ src/main/java/edu/ucsb/cs56/mapache_search/entities/User.java
@@ -1,9 +1,9 @@
 package edu.ucsb.cs56.mapache_search.user;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
 public class User {
