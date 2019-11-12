3ccb879b75ddf70b98c7abd475ba2a3bf60baee6
Ben Ye <z_ye@ucsb.edu>
2019-11-12 01:27:40 -0800

    user settings template

---
+++ src/main/java/edu/ucsb/cs56/mapache_search/user/User.java
@@ -0,0 +1,29 @@
+package edu.ucsb.cs56.mapache_search.user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+public class User {
+
+    @NotBlank(message = "Google search API Key is necessary")
+    private String apiKey;
+
+    @NotBlank(message = "Last Name is mandatory")
+    private String username;
+
+    public User(String username, String apiKey) {
+        this.username = username;
+        this.apiKey = apiKey;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getAPIKey() {
+        return this.apiKey;
+    }
+}
