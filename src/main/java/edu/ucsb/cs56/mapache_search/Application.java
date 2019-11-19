7ca986f183a983231a95c62edb7a0896bb233509
Ben Ye <z_ye@ucsb.edu>
2019-11-18 21:38:12 -0800

    Configured Application.java

--- src/main/java/edu/ucsb/cs56/mapache_search/Application.java
+++ src/main/java/edu/ucsb/cs56/mapache_search/Application.java
@@ -2,13 +2,32 @@ package edu.ucsb.cs56.mapache_search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableOAuth2Sso
 @SpringBootApplication
-public class Application {
+public class Application extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        .authorizeRequests()
+            .antMatchers("/","/login**","/webjars/**","/error**")
+            .permitAll()
+        .anyRequest()
+            .authenticated()
+        .and()
+            .oauth2Login().loginPage("/login")
+        .and()
+            .logout()
+            .deleteCookies("remove")
+            .invalidateHttpSession(true)
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/")
+            .permitAll();
+    }
 }
