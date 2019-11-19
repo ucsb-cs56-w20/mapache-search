b6f36338a251f513b2643dcef633c84bddaab032
Bryan Terce <bryan.terce@gmail.com>
2019-11-19 15:52:57 -0800

    Make spring-boot load localhost.json

--- src/main/java/edu/ucsb/cs56/mapache_search/Application.java
+++ src/main/java/edu/ucsb/cs56/mapache_search/Application.java
@@ -3,9 +3,11 @@ package edu.ucsb.cs56.mapache_search;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.PropertySource;
 
 @EnableOAuth2Sso
 @SpringBootApplication
+@PropertySource(value = "localhost.json", factory = JsonPropertySourceFactory.class, ignoreResourceNotFound = true)
 public class Application {
 
 	public static void main(String[] args) {
