8e14ad9ea2ae4f30744acbcd5d5ca86d4bf40a2b
Bryan Terce <bryan.terce@gmail.com>
2019-11-27 23:04:26 -0800

    Make GoogleSearchService use urivariables map

--- src/main/java/edu/ucsb/cs56/mapache_search/GoogleSearchService.java
+++ src/main/java/edu/ucsb/cs56/mapache_search/GoogleSearchService.java
@@ -2,6 +2,8 @@ package edu.ucsb.cs56.mapache_search;
 
 import java.security.Principal;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,13 +36,8 @@ public class GoogleSearchService implements SearchService {
     public GoogleSearchService() {
     }
 
-    /*public GoogleSearchService(String apiKey, String searchId) {
-        this.apiKey = apiKey;
-        logger.info("apiKey=" + apiKey);
-
-        this.searchId = searchId;
-        logger.info("searchId=" + searchId);
-    }*/
+    private static final String SEARCH_ENDPOINT =
+        "https://www.googleapis.com/customsearch/v1?key={key}&cx={searchId}&q={query}&alt={outputFormat}";
 
     public String getJSON(String query, String apiKey) {
         logger.info("apiKey=" + apiKey);
@@ -48,24 +45,23 @@ public class GoogleSearchService implements SearchService {
         RestTemplate restTemplate = new RestTemplate();
 
         HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
         headers.setContentType(MediaType.APPLICATION_JSON);
-        //headers.set("ucsb-api-version", "1.0");
-        //headers.set("ucsb-api-key", this.apiKey);
 
         HttpEntity<String> entity = new HttpEntity<>("body", headers);
 
-        String uri = "https://www.googleapis.com/customsearch/v1";
-        String params = String.format("?key=%s&cx=%s&q=%s&alt=json",
-            apiKey, searchId, query);
-        String url = uri + params;
-        logger.info("url=" + url);
+        Map<String, String> uriVariables =
+            Map.of(
+                "key", apiKey,
+                "searchId", searchId,
+                "query", query,
+                "outputFormat", "json"
+            );
 
-        String retVal="";
+        String retVal = "";
         try {
-            ResponseEntity<String> re = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-             MediaType contentType = re.getHeaders().getContentType();
-            HttpStatus statusCode = re.getStatusCode();
+            ResponseEntity<String> re =
+                restTemplate.exchange(SEARCH_ENDPOINT, HttpMethod.GET, entity, String.class, uriVariables);
             retVal = re.getBody();
         } catch (HttpClientErrorException e) {
             retVal = "{\"error\": \"401: Unauthorized\"}";
