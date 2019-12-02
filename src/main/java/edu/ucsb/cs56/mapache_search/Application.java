2f291142193f0aca80be8e0b1bd9f72b52d97c4c
Darragh Burke <darragh@ucsb.edu>
2019-12-02 12:26:27 -0800

    db remove unneeded import

--- src/main/java/edu/ucsb/cs56/mapache_search/Application.java
+++ src/main/java/edu/ucsb/cs56/mapache_search/Application.java
@@ -3,7 +3,6 @@ package edu.ucsb.cs56.mapache_search;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
