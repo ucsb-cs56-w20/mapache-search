a792d43716c541a164921e6ef8151e04275f9422
Matthew Li <mli01@umail.ucsb.edu>
2019-12-03 11:02:15 -0800

    ml - setup entity, repository, and controller for Votes

---
+++ src/main/java/edu/ucsb/cs56/mapache_search/entities/UserVote.java
@@ -0,0 +1,30 @@
+package edu.ucsb.cs56.mapache_search.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UserVote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private boolean upvote;
+    private AppUser user;
+    private SearchResultEntity result;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public boolean getUpvote() { return upvote; }
+    public void setUpvote(boolean upvote) { this.upvote = upvote; }
+
+    public AppUser getUser() { return user; }
+    public void setUser(AppUser user) { this.user = user; }
+    
+    public SearchResultEntity getResult() { return result; }
+    public void setResult(SearchResultEntity result) { this.result = result; }     
+
+}
