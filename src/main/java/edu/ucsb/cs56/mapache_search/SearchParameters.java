7034f0442c86974be3bca7c320331273130de8f2
Bryan Terce <bryan.terce@gmail.com>
2019-12-03 18:47:50 -0800

    Replace SearchParameters builder with simple java object

--- src/main/java/edu/ucsb/cs56/mapache_search/SearchParameters.java
+++ src/main/java/edu/ucsb/cs56/mapache_search/SearchParameters.java
@@ -1,52 +1,22 @@
 package edu.ucsb.cs56.mapache_search;
 
 public class SearchParameters {
-    private final String query;
-    private final int page;
-
-    private SearchParameters(String query, int page) {
-        this.query = query;
-        this.page = page;
-    }
-
-    public static Builder builder() {
-        return new Builder();
-    }
-
-    public static class Builder {
-        private String query = "";
-        private int page = 1;
-
-        private Builder(SearchParameters params) {
-            query = params.query;
-            page = params.page;
-        }
-
-        private Builder() {}
-
-        public SearchParameters build() {
-            return new SearchParameters(query, page);
-        }
-
-        public Builder setQuery(String query) {
-            this.query = query;
-            return this;
-        }
-
-        public Builder setPage(int page) {
-            if (page < 1 || page > 10) {
-                throw new IllegalArgumentException("Page must be a value between 1 and 10 inclusive");
-            }
-            this.page = page;
-            return this;
-        }
-    }
+    private String query;
+    private int page = 1;
 
     public String getQuery() {
         return query;
     }
 
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
     public int getPage() {
         return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
     }
 }
