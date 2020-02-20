44a3c85585857b4663e6a4e4855a245569ce1364
Jared Flores <jared_flores@cs.ucsb.edu>
2020-02-20 11:25:51 -0800

    jf - Removed unnecessary echo from env.bat

--- env.bat
+++ env.bat
@@ -2,9 +2,6 @@
 
 set SPRING_APPLICATION_JSON={
 
-rem *** Two empty lines are required for the linefeed
 FOR /F "skip=1 delims=" %%a in (localhost.json) do (
   call set "SPRING_APPLICATION_JSON=%%SPRING_APPLICATION_JSON%%%%a"
 )
-
-echo %SPRING_APPLICATION_JSON%
