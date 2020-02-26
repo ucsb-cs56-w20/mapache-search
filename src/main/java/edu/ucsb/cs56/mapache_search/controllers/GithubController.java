9e4082f3eb388a9afe73b07cb763a830bb8092b3
Matthew Chen <matthew@shorttimer.com>
2020-02-26 12:54:48 -0800

    mc - made github page, still very hacky

---
+++ src/main/java/edu/ucsb/cs56/mapache_search/controllers/GithubController.java
@@ -0,0 +1,33 @@
+package edu.ucsb.cs56.mapache_search.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import edu.ucsb.cs56.mapache_search.membership.AuthControllerAdvice;
+import edu.ucsb.cs56.mapache_search.membership.GithubOrgMembershipService;
+
+@Controller
+public class GithubController {
+
+    private Logger logger = LoggerFactory.getLogger(SearchController.class);
+
+    @Autowired
+    private AuthControllerAdvice controllerAdvice;
+
+    @GetMapping("/github")
+    public String gitHub(Model model, OAuth2AuthenticationToken token) {
+        logger.info(controllerAdvice.toString());
+        if(controllerAdvice.getIsAdmin(token) || controllerAdvice.getIsMember(token)){
+            return "github";
+        }
+        else{
+            return "/login";
+        }
+    }
+}
