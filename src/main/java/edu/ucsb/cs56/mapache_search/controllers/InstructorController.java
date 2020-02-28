ac12e472dbdafdba81bf314c7cb3b11901b8c222
Francie Wei <hwei@cs.ucsb.edu>
2020-02-28 00:19:57 -0800

    fw/cs - fix some bugs in InstructorController, still have some error in Optional<>

--- src/main/java/edu/ucsb/cs56/mapache_search/controllers/InstructorController.java
+++ src/main/java/edu/ucsb/cs56/mapache_search/controllers/InstructorController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import edu.ucsb.cs56.mapache_search.entities;
-import edu.ucsb.cs56.mapache_search.repositories;
+import edu.ucsb.cs56.mapache_search.entities.AppUser;
+import edu.ucsb.cs56.mapache_search.repositories.UserRepository;
 import java.util.Optional;
 import javax.validation.Valid;
 import java.util.List;
@@ -20,8 +20,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class InstructorController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    public InstructorController(UserRepository repo) {
+        this.userRepository = repo;
+    }
 @PostMapping("/instructorDashboard/delete/{uid}")
-    public String deleteViewer(@PathVariable("id") long uid, Model model,
+    public String deleteViewer(@PathVariable("id") String uid, Model model,
             RedirectAttributes redirAttrs, AppUser user) {
         if (!user.getIsInstructor()) {
             redirAttrs.addFlashAttribute("alertDanger",
@@ -29,34 +36,33 @@ public class InstructorController {
             return "redirect:/";
         }
 
-        Optional<AppUser> appUser = UserRepository.findByUid(uid);
+        Optional<AppUser> appUser = userRepository.findByUid(uid).get(0);
         if (!appUser.isPresent()) {
             redirAttrs.addFlashAttribute("alertDanger", "Instructor with that uid does not exist.");
         } else {
-            String uid = appUser.getUid();
-            UserRepository.findByUid(uid).setIsInstructor(false);
+            userRepository.findByUid(uid).get(0).setIsInstructor(false);
             redirAttrs.addFlashAttribute("alertSuccess", "Instructor successfully deleted.");      
         }
         return "redirect:/instructorDashboard/addInstructor";
     }
 
     @PostMapping("/instructorDashboard/add")
-    public String addAdmin(@Valid Admin admin, BindingResult result, Model model,
+    public String addInstructor(@Valid AppUser user, BindingResult result, Model model,
             RedirectAttributes redirAttrs, OAuth2AuthenticationToken token) {
         if (!user.getIsInstructor()) {
             redirAttrs.addFlashAttribute("alertDanger",
                     "You do not have permission to access that page");
             return "redirect:/";
         }
-        Optional<AppUser> appUser = UserRepository.findByUid(uid);
+        Optional<AppUser> appUser = userRepository.findByUid(user.getUid()).get(0);
         if (!appUser.isPresent()) {
             redirAttrs.addFlashAttribute("alertDanger", "User with that uid does not exist.");
         } else {
-            String uid = appUser.getUid();
+            String uid = appUser.get().getUid();
             if (appUser.get().getIsInstructor()) {
                 redirAttrs.addFlashAttribute("alertDanger", "User " + uid + " has already been an Instructor.");    
             } else {
-                UserRepository.findByUid(uid).setViewer(true);
+                appUser.get().setIsInstructor(true);
                 redirAttrs.addFlashAttribute("alertSuccess", "Instructor successfully added.");    
             }
         }
