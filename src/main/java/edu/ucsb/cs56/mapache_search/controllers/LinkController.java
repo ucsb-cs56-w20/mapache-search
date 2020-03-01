0896153459e86284305b3da9a51ee4e3f0b771a5
Matthew Chen <matthew@shorttimer.com>
2020-02-29 16:45:34 -0800

    Made search results redirect to /link endpoint and added logger message for redirect

--- src/main/java/edu/ucsb/cs56/mapache_search/controllers/LinkController.java
+++ src/main/java/edu/ucsb/cs56/mapache_search/controllers/LinkController.java
@@ -1,5 +1,7 @@
 package edu.ucsb.cs56.mapache_search.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,10 +10,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class LinkController {
 
+    private Logger logger = LoggerFactory.getLogger(SearchController.class);
     
     @GetMapping("/link")
-    //    @ResponseBody
     public String redirect(@RequestParam(name = "url", required = true) String url) {
-	return "redirect:" + url;
+        logger.info("Redirecting to: "  + url);
+	    return "redirect:" + url;
     }
 }
