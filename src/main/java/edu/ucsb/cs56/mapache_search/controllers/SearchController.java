7028c12634c88557a7570833b0419b2f76ceef8d
trungbui2000 <trungbuil@cs.ucsb.edu>
2020-03-04 14:57:37 -0800

    tb-removing the place holder for the team numbers on the top navigation bar

--- src/main/java/edu/ucsb/cs56/mapache_search/controllers/SearchController.java
+++ src/main/java/edu/ucsb/cs56/mapache_search/controllers/SearchController.java
@@ -153,9 +153,6 @@ public class SearchController {
         }
 
         String json = searchService.getJSON(params, apiKey);
-        model.addAttribute("searchResultQueryFound",params.getQuery());
-        
-
         SearchResult sr = SearchResult.fromJSON(json);
 
         if(sr.getKind() != "error") {
