7db7df8e134fdf7852bda75be572406b46b39c4e
eduardogalvez <eduardogalvez@ucsb.edu>
2020-03-05 16:54:24 -0800

    eg/bk - able to save link data: user id , time , and url

--- src/main/java/edu/ucsb/cs56/mapache_search/controllers/LinkController.java
+++ src/main/java/edu/ucsb/cs56/mapache_search/controllers/LinkController.java
@@ -5,15 +5,55 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import edu.ucsb.cs56.mapache_search.membership.AuthControllerAdvice;
+import edu.ucsb.cs56.mapache_search.controllers.BasicErrorController;
+import edu.ucsb.cs56.mapache_search.entities.Link;
+import edu.ucsb.cs56.mapache_search.entities.AppUser;
+import edu.ucsb.cs56.mapache_search.repositories.UserRepository;
+import edu.ucsb.cs56.mapache_search.repositories.LinkRepository;
+import java.util.Date;
 
 @Controller
 public class LinkController {
 
     private Logger logger = LoggerFactory.getLogger(SearchController.class);
+
+    @Autowired
+    private AuthControllerAdvice controllerAdvice;
+
+    @Autowired
+    private BasicErrorController signInError;
+
+    @Autowired
+    private LinkRepository linkRepository;
+
+    @Autowired
+    private UserRepository userRepository;
     
     @GetMapping("/link")
-    public String redirect(@RequestParam(name = "url", required = true) String url) {
+    public String redirect(@RequestParam(name = "url", required = true) String url, OAuth2AuthenticationToken token) {
         logger.info("Redirecting to: "  + url);
-	return "redirect:" + url;
+
+    
+    
+	if (controllerAdvice.getIsAdmin(token) || controllerAdvice.getIsMember(token)) {
+        AppUser user = userRepository.findByUid(controllerAdvice.getUid(token)).get(0);
+
+         Link userLink = new Link();
+        userLink.setUserId(user);
+        userLink.setUrl(url);
+                userLink.setTimestamp(new Date());
+
+            linkRepository.save(userLink);
+
+
+	    return "redirect:" + url;
+	} else {
+	    return signInError.loginError();
+	}
     }
 }
