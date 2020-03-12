d12faa059dbd71cbc45f26db304dc819e0fba144
FrancieNagiki <hwei@umail.ucsb.edu>
2020-03-11 21:43:19 -0700

    fw - import  RedirectAttributes;

--- src/main/java/edu/ucsb/cs56/mapache_search/controllers/UserController.java
+++ src/main/java/edu/ucsb/cs56/mapache_search/controllers/UserController.java
@@ -15,6 +15,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 import edu.ucsb.cs56.mapache_search.entities.AppUser;
 import edu.ucsb.cs56.mapache_search.entities.UserVote;
