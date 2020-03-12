3575f8bee066ee8397681a2f439835c79e3f2a6d
coreysucs <46921597+coreysucs@users.noreply.github.com>
2020-03-12 13:34:30 -0700

    cs - merge updates from master

--- src/main/java/edu/ucsb/cs56/mapache_search/controllers/InstructorController.java
+++ src/main/java/edu/ucsb/cs56/mapache_search/controllers/InstructorController.java
@@ -28,14 +28,17 @@ import org.slf4j.LoggerFactory;
 
 @Controller
 public class InstructorController {
-
-    static final Logger logger = LoggerFactory.getLogger(InstructorController.class);
+    @Autowired
+    private MembershipService ms;
 
     @Autowired
     private UserRepository userRepository;
-    
+
     @Autowired
-    private AuthControllerAdvice controllerAdvice;
+    private VoteRepository voteRepository;
+
+    @Autowired
+    private SearchTermsRepository searchtermsRepository;
 
     @Autowired
     public InstructorController(UserRepository repo) {
@@ -59,7 +62,64 @@ public class InstructorController {
         }
         model.addAttribute("searchCount",amountSearched);
         return "instructor/index";
-    }  
+    }
+    
+    @GetMapping("instructor/data")
+    public String data(Model model, RedirectAttributes redirAttrs, AppUser user, OAuth2AuthenticationToken token) {
+        String role = ms.role(token);
+        if (!role.equals("Admin")) {
+            redirAttrs.addFlashAttribute("alertDanger",
+                    "You do not have permission to access that page");
+            return "redirect:/";
+        }
+        return "instructor/data_stub";
+    }
+
+    @GetMapping("instructor/upvotes")
+    public String upvotes(Model model, RedirectAttributes redirAttrs, AppUser user, OAuth2AuthenticationToken token) {
+        String role = ms.role(token);
+        if (!role.equals("Admin")) {
+            redirAttrs.addFlashAttribute("alertDanger",
+                    "You do not have permission to access that page");
+            return "redirect:/";
+        }
+        List<UserVote> upVoteList = voteRepository.findAll();
+        ArrayList<searchUpVotedWrapper> upVotedSearches = new ArrayList<>();
+        for(int pos = 0; pos < upVoteList.size(); pos++) {
+            if (pos > 100) break;
+            UserVote vote = upVoteList.get(pos);
+            upVotedSearches.add(new searchUpVotedWrapper(vote.getResult()));
+        }
+        Collections.sort(upVotedSearches);
+        int x = upVotedSearches.size();
+        model.addAttribute("upVotedSearchesSize",x);
+        model.addAttribute("upVotedSearches", upVotedSearches);
+
+        return "instructor/upvote_page";
+    }
+    
+    @GetMapping("instructor/popular_searches")
+    public String searches(Model model, RedirectAttributes redirAttrs, AppUser user, OAuth2AuthenticationToken token) {
+        String role = ms.role(token);
+        if (!role.equals("Admin")) {
+            redirAttrs.addFlashAttribute("alertDanger",
+                    "You do not have permission to access that page");
+            return "redirect:/";
+        }
+        List<SearchTerms> searchTermsList = searchtermsRepository.findAll();
+        ArrayList<searchedTermsWrapper> searchedTerms = new ArrayList<>();
+        for(int pos = 0; pos < searchTermsList.size(); pos++) {
+            if (pos > 100) break;
+            SearchTerms searched = searchTermsList.get(pos);
+            searchedTerms.add(new searchedTermsWrapper(searched));
+        }
+        Collections.sort(searchedTerms);
+        int x = searchedTerms.size();
+        model.addAttribute("searchedTermsSize",x);
+        model.addAttribute("searchedTerms", searchedTerms);
+
+        return "instructor/popular_searches";
+    }
     
     @PostMapping("/instructor/delete/{username}")
     public String deleteViewer(@PathVariable("username") String username, Model model,
@@ -113,6 +173,52 @@ public class InstructorController {
     @GetMapping("/instructor/random_student_generator")
     public String getRandomStudent(Model model, OAuth2AuthenticationToken token){
         return "instructor/random_student_generator";
+    }
+    
+    public class searchUpVotedWrapper implements Comparable<searchUpVotedWrapper> {
+        private SearchResultEntity result;
+
+        public searchUpVotedWrapper(SearchResultEntity result) {
+            this.result = result;
+        }
+
+        public SearchResultEntity getResult() {
+            return result;
+        }
+
+        public int compareTo(searchUpVotedWrapper objSearch) {
+            if (getResult().getVotecount() > objSearch.getResult().getVotecount()) {
+                return 1;
+            }
+            else if (getResult().getVotecount() < objSearch.getResult().getVotecount()) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    public class searchedTermsWrapper implements Comparable<searchedTermsWrapper> {
+        private SearchTerms result;
+
+        public searchedTermsWrapper(SearchTerms result) {
+            this.result = result;
+        }
+
+        public SearchTerms getResult() {
+            return result;
+        }
+
+        public int compareTo(searchedTermsWrapper objSearch) {
+            if (getResult().getCount() > objSearch.getResult().getCount()) {
+                return 1;
+            }
+            else if (getResult().getCount() < objSearch.getResult().getCount()) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
     }
 
 }
